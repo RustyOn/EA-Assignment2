d0056db01df5743f21488fc71340f80b9ef30404
RustyOn <98265365+RustyOn@users.noreply.github.com>
2023-07-13 08:15:57 +0200

    LoginPageSkeleton
    feat: created the basic LoginPage skeleton

---
+++ ea-assignment2/src/InputDisplay.jsx
@@ -0,0 +1,9 @@
+
+function InputDisplay(){
+
+    return(
+        <></>
+    )
+}
+
+export default InputDisplay
---
+++ ea-assignment2/src/LoginPage.jsx
@@ -0,0 +1,12 @@
+import InputDisplay from "./InputDisplay"
+
+
+function LoginPage(){
+    return(
+        <>
+            <InputDisplay />
+        </>
+    )
+}
+
+export default LoginPage
