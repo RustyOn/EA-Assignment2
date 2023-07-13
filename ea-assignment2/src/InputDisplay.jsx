4bc26833e3fe23fe2bbcb2017b938f63dac1ddd5
RustyOn <98265365+RustyOn@users.noreply.github.com>
2023-07-13 08:27:21 +0200

    Update InputDisplay
    feat: data from input form can be displayed in console for testing purposes

--- ea-assignment2/src/InputDisplay.jsx
+++ ea-assignment2/src/InputDisplay.jsx
@@ -1,9 +1,26 @@
+function InputDisplay() {
 
-function InputDisplay(){
+    function handleSubmit(e){
+        e.preventDefault()
 
-    return(
-        <></>
-    )
+        const form = e.target
+        const formData = new FormData(form)
+
+        const formJson = Object.fromEntries(formData.entries())
+        console.log(formJson)
+    }
+
+  return (
+    <>
+      <form method="post" onSubmit={handleSubmit}>
+        <label>
+          Username: <input name="userName" />
+        </label>
+
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
 }
 
-export default InputDisplay
+export default InputDisplay;
