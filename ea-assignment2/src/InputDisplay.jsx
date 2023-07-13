940b03f62dd4fa82cf9801d599ae7dddfe906a99
RustyOn <98265365+RustyOn@users.noreply.github.com>
2023-07-13 13:31:50 +0200

    API POST tests
    feat: testing API POSTing
    
    note: currently seems to be broken and post multiple times the same string, gonna troubleshoot it today cuz I think I know what the issue is

--- ea-assignment2/src/InputDisplay.jsx
+++ ea-assignment2/src/InputDisplay.jsx
@@ -1,42 +1,54 @@
-import { useEffect, useState } from "react"
-import {API_URL} from "./utils"
+import { useEffect, useState } from "react";
+import { API_URL, API_KEY } from "./utils";
 
 function InputDisplay() {
+  const [jsonData, setJsonData] = useState([]);
 
-  const [jsonData, setJsonData] = useState([]) 
-
-  useEffect(() =>{
+  useEffect(() => {
     fetch(API_URL)
-    .then(response => response.json())
-    .then(json => {
-      console.log(json)
-      setJsonData(json)
-    })
-    .catch(error => console.error(error.message))
-  }, [])
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setJsonData(json);
+      })
+      .catch((error) => console.error(error.message));
+  }, []);
 
-    function handleSubmit(e){
-        e.preventDefault()
+  function handleSubmit(e) {
+    e.preventDefault();
 
-        const form = e.target
-        const formData = new FormData(form)
+    const form = e.target;
+    const formData = new FormData(form);
 
-        const formJson = Object.fromEntries(formData.entries())
-        console.log(formJson)
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
 
-        if(!jsonData)
-          return
-        if(jsonData){
-          jsonData.map(data =>
-            {if(data.username === formJson.userName){
-              return console.log("User exists")
-            }
-            return console.log("Does not exist")
+    if (!jsonData) return;
+    if (jsonData) {
+      jsonData.map((data) => {
+        if (data.username === formJson.userName) {
+          return console.log("User exists");
+        }else{
+
+          fetch(API_URL, {
+            method: "POST",
+            headers: {
+              'X-API-Key': API_KEY,
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              username: formJson.userName,
+              translations: []
+            })
+    
+        })
           }
-            )
-        }
+          
+        
+        return console.log("Does not exist");
+      });
     }
-
+  }
 
   return (
     <>
@@ -48,7 +60,8 @@ function InputDisplay() {
         <button type="submit">Login</button>
       </form>
 
-      {jsonData && jsonData.map(data => <h3 key={data.id}>{data.username}</h3>)}
+      {jsonData &&
+        jsonData.map((data) => <h3 key={data.id}>{data.username}</h3>)}
     </>
   );
 }
--- ea-assignment2/src/utils.js
+++ ea-assignment2/src/utils.js
@@ -1,1 +1,2 @@
 export const API_URL ="https://ea-assignment2-production.up.railway.app/translations"
+export const API_KEY ="qwertyuiop1234567890"
