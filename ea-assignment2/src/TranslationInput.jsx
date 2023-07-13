ccbb8636e579cc8a6b7d90cd16ce4361116ffd56
AnnikaZar <annika.zarske@se.experis.com>
2023-07-13 15:08:34 +0200

    translation input box
    feat: basic input box for translation

--- ea-assignment2/src/TranslationInput.jsx
+++ ea-assignment2/src/TranslationInput.jsx
@@ -1,8 +1,25 @@
-
+import { useState } from "react"
 
 function TranslationInput(){
+    const [ text, setText ] = useState({value: ""})
+
+    const handleTextChange = event =>{
+        setText({ value: event.target.value })
+    }
+
+    const handleSubmit = event =>{
+        event.preventDefault()
+        console.log("thi text was entered: " + text.value)
+        //TODO: connect to translationHandler
+    }
+
     return(
-        <></>
+        <form onSubmit={ handleSubmit }>
+            <fieldset>
+                <input type="text" value={ text.value } onChange= { handleTextChange }/>
+                <button type="submit">Translate</button>
+            </fieldset>
+        </form>
     )
 }
 
