aac1aeabc3adf6c14d64b995ce68fbbe72142624
RustyOn <98265365+RustyOn@users.noreply.github.com>
2023-07-14 13:04:30 +0200

    Update App.jsx
    feat: nav list is now only visible when logged in, can no longer navigate back to the login page
    
    todo: logout function to get back to login screen

--- ea-assignment2/src/App.jsx
+++ ea-assignment2/src/App.jsx
@@ -2,25 +2,36 @@ import logo from "./logo.svg";
 import "./App.css";
 import LoginPage from "./LoginPage";
 import TranslationPage from "./TranslationPage";
-import Page404 from "./Page404"
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Page404 from "./Page404";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink,
+  useLocation,
+} from "react-router-dom";
+
+function Nav() {
+  const currPath = useLocation();
+  if (currPath.pathname === "/") return null;
+
+  return (
+    <nav>
+      <li>
+        <NavLink to="/translation">Translation</NavLink>
+      </li>
+      <li>
+        <NavLink to="/profile">Profile</NavLink>
+      </li>
+    </nav>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <nav>
-          <li>
-            <NavLink to="/">Login</NavLink>
-          </li>
-          <li>
-            <NavLink to="/translation">Translation</NavLink>
-          </li>
-          <li>
-            <NavLink to="/profile">Profile</NavLink>
-          </li>
-        </nav>
-
+        <Nav />
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/translation" element={<TranslationPage />} />
