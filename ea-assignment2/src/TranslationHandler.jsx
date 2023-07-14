921d6f39eb543c9645a25a3c1d66ea1c6a72e999
AnnikaZar <annika.zarske@se.experis.com>
2023-07-14 09:18:53 +0200

    imageImport
    feat: importing images (not working)

--- ea-assignment2/src/TranslationHandler.jsx
+++ ea-assignment2/src/TranslationHandler.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react"
+//import img from './images/a.png'
+const images = require.context('./images', true, /\.(png)$/)
+const imageList = images.keys().map(image => images(image))
 
 // TODO: 
 // connect to symbol library
-// Split words into single symbols
+// --Split words into single symbols--
 // return correct symbols
 
 function TranslationHandler(){
     const [ text, setText ] = useState({value: ""})
+    let textSymbolsArray = []
 
     const handleTextChange = event =>{
         setText({ value: event.target.value })
@@ -15,30 +19,40 @@ function TranslationHandler(){
     const handleSubmit = event =>{
         event.preventDefault()
         console.log("this text was entered: " + text.value)
+         textSymbolsArray = textToTranslate.split("")
+        console.log(textSymbolsArray)
     }
     
     let textToTranslate = text.value
-    const textSymbolsArray = textToTranslate.split("")
-    console.log(textSymbolsArray)
+    
+    
     let symbolsToPrint = []
     textSymbolsArray.forEach(symbol => {
+
         //TODO: compare symbols to database and add to print
         //loop through to compare?
         //return symbol if match found
         //if no match - ignore? / remove from array
     });
 
+
+    //<img src={img} alt="Logo" />
     return(
         <>
             <form onSubmit={ handleSubmit }>
                 <fieldset>
                     <input type="text" value={ text.value } onChange= { handleTextChange }/>
-                    <button type="submit" disabled>Translate</button>
+                    <button type="submit" >Translate</button>
                 </fieldset>
             </form>
             <p>
                 Text translation
             </p>
+            <div>
+                {imageList.map((image, index) => (
+                    <img key={index} src={image.default} alt={`image-${index}`} />
+                ))}
+            </div>
         </>
         
     )
