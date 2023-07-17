41940393ac666821307985fc0f71b19ce8d013bb
RustyOn <98265365+RustyOn@users.noreply.github.com>
2023-07-17 15:32:00 +0200

    Started on the Profile Page
    feat: fetching API and displaying translation data
    
    not fully working, keeps displaying data from the first user

--- ea-assignment2/src/DisplayUser.jsx
+++ ea-assignment2/src/DisplayUser.jsx
@@ -1,6 +1,9 @@
-import {useState, useEffect} from "react"
-import {API_URL} from "./utils.js"
+import { useState, useEffect } from "react";
+import { API_URL } from "./utils.js";
+import ProfileItem from "./ProfileItem.jsx";
 
+
+// Not working properly yet, keeps showing the translations from the first user even when logged in as another user
 function DisplayUser() {
   let currUser = sessionStorage.getItem("currUser");
   const [jsonData, setJsonData] = useState([]);
@@ -13,19 +16,21 @@ function DisplayUser() {
       .catch((error) => console.error(error.message));
   }, []);
 
-  for(let i = 0; i < jsonData.length; i++){
-    if(currUser === jsonData[i].username){
-        return(
-            <p>{jsonData[i].translations}</p>
-        )
-    }else{
-        return(
-            <p>No translations found</p>
-        )
+  for (let i = 0; i < jsonData.length; i++) {
+    for (let j = 0; j < jsonData[i].translations.length; j++) {
+      if (
+        currUser === jsonData[i].username &&
+        jsonData[i].translations !== null
+      ) {
+        console.log(jsonData[i].translations[j]);
+      }
     }
-
+    return (
+      <>
+        <p>{jsonData[i].translations}</p>
+      </>
+    );
   }
-
 }
 
 export default DisplayUser;
---
+++ ea-assignment2/src/ProfileItem.jsx
@@ -0,0 +1,10 @@
+
+/*
+export default function ProfileItem(translation){
+    let toPrint = translation
+    return(
+        <>
+        <p>{toPrint}</p>
+        </>
+    )
+}*/
