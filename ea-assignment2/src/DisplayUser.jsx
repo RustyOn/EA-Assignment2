4bf986002c213bcba0e32f28b2c5187554b632d4
RustyOn <98265365+RustyOn@users.noreply.github.com>
2023-07-18 09:10:16 +0200

    Update DisplayUser.jsx
    feat: display each translation in a list

--- ea-assignment2/src/DisplayUser.jsx
+++ ea-assignment2/src/DisplayUser.jsx
@@ -2,39 +2,33 @@ import { useState, useEffect } from "react";
 import { API_URL } from "./utils.js";
 import ProfileItem from "./ProfileItem.jsx";
 
-
-// Not working properly yet, keeps showing the translations from the first user even when logged in as another user
 function DisplayUser() {
   let currUser = sessionStorage.getItem("currUser");
   const [jsonData, setJsonData] = useState([]);
-  let runOnce = false
+  let runOnce = false;
   useEffect(() => {
-    if(!runOnce){
-        fetch(API_URL)
+    if (!runOnce) {
+      fetch(API_URL)
         .then((response) => response.json())
         .then((json) => {
           setJsonData(json);
         })
         .catch((error) => console.error(error.message));
-        runOnce = true
-    }      
+      runOnce = true;
+    }
   }, []);
 
   for (let i = 0; i < jsonData.length; i++) {
-    for (let j = 0; j < jsonData[i].translations.length; j++) {
-      if (
-        currUser === jsonData[i].username &&
-        jsonData[i].translations !== null
-      ) {
-        console.log(jsonData[i].translations[j]);
-        return (
-            <>
-              <p>{jsonData[i].translations}</p>
-            </>
-          );
-      }
+    if (
+      currUser === jsonData[i].username &&
+      jsonData[i].translations !== null
+    ) {
+      //Key of the element is the array index from map - not the best solution but it gets the job done for now
+      const listTrans = jsonData[i].translations.map((translation, index) => (
+        <li key={index}>{translation}</li>
+      ));
+      return <ul>{listTrans}</ul>;
     }
-   
   }
 }
 
