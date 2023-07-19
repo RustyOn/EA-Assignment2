a8b7fae39d6dcf7fa6a3c1c1b819ccdbd07f50ad
RustyOn <98265365+RustyOn@users.noreply.github.com>
2023-07-19 09:35:54 +0200

    Input Display Comments
    chore: began the process of adding comments

--- ea-assignment2/src/InputDisplay.jsx
+++ ea-assignment2/src/InputDisplay.jsx
@@ -3,17 +3,17 @@ import { API_URL } from "./utils";
 import { useNavigate } from "react-router-dom";
 import PostAPI from "./PostAPI";
 
-
+//checks if a user exists in the supplied JSON data
+  //If a user exists save their array index as a user ID and break out of the function
+  //If a user does not exist continue iterating through the data, finally return false if they do not exist at all
+  //This could very well be put into a separate component
 function checkUserExist(userName, jsonData) {
   let userExists = false;
-  let currID = 0
   if(userName === "" || userName === null) return (userExists = false)
-  
   if (jsonData !== []) {
     for (let i = 0; i < jsonData.length; i++) {
       if (jsonData[i].username === userName) {
-        currID = i
-        sessionStorage.setItem("currID", currID)
+        sessionStorage.setItem("currID", i)
         return (userExists = true);
       } else {
         userExists = false;
@@ -27,6 +27,7 @@ function InputDisplay() {
   const [jsonData, setJsonData] = useState([]);
   const navigate = useNavigate();
   let currUser = sessionStorage.getItem("currUser");
+  //Fetching the API and storing it in a useState
   useEffect(() => {
     fetch(API_URL)
       .then((response) => response.json())
@@ -36,32 +37,40 @@ function InputDisplay() {
       .catch((error) => console.error(error.message));
   }, []);
 
+  //As soon as this component renders check if there is a currUser, if yes go straight to Translation Page
   useEffect(() => {
     if (currUser) {
       navigate("/translations");
     }
   }, []);
 
+  //Using useRef to not update the input every time the user enters a value, instead the input is set on submit
   const inputRef = useRef();
 
+  //On submitting the form check if jsonData is empty and return if that is true
   const handleSubmit = (event) => {
     event.preventDefault();
     if (!jsonData) return;
     let userName = inputRef.current.value;
+    //Sets the inputted username to the session storage
     sessionStorage.setItem("currUser", userName);
 
+    //Guards against an empty input (This is not needed anymore as we've made the input field required) 
     if(userName === ''){
       console.log("Input is empty")
     }else{
       if (checkUserExist(userName, jsonData) === false) {
+        //If user does not exist then a new user will be created
         console.log("Does not exist");
-        //PostAPI(userName)
+        //PostAPI(userName) !!!!!!!!!!!
         console.log("Added new user");
        
       } else {
+        //Nothing special happens if a user does exist
         console.log("Exists");
 
       }
+      //Whichever the case always navigates to the Translation Page
       navigate("/translation");
     }
   }
--- ea-assignment2/src/LoginPage.jsx
+++ ea-assignment2/src/LoginPage.jsx
@@ -1,21 +1,11 @@
-import { useEffect } from "react"
+
 import InputDisplay from "./InputDisplay"
-import {useNavigate} from "react-router-dom"
-
-
 function LoginPage(){
-
-
-
-    
         return(
             <>
             <InputDisplay />
             </>
         )
-
-       
-    
 }
 
 export default LoginPage
