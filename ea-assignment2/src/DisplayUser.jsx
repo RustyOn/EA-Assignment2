97078c28a0e8ae5c7cb55c23332c2ded99783a3e
RustyOn <98265365+RustyOn@users.noreply.github.com>
2023-07-19 09:43:37 +0200

    Update DisplayUser.jsx
    chore: added comments to DisplayUser.jsx

--- ea-assignment2/src/DisplayUser.jsx
+++ ea-assignment2/src/DisplayUser.jsx
@@ -3,13 +3,17 @@ import { API_URL } from "./utils.js";
 import DeleteAPI from "./DeleteAPI.jsx";
 
 function DeleteTrans() {
-  //DeleteAPI()
+  //DeleteAPI() !!!!!!!!!!!!!!!!!
 }
 
 function DisplayUser() {
   let currUser = sessionStorage.getItem("currUser");
   const [jsonData, setJsonData] = useState([]);
   let runOnce = false;
+  //Fetching the API and storing the data in a useState
+    //We've had the useEffect trigger twice for some reason, possible re-render from further up?
+    //Never found the actual cause
+    //runOnce makes sure that it fetches the data only once
   useEffect(() => {
     if (!runOnce) {
       fetch(API_URL)
@@ -22,12 +26,16 @@ function DisplayUser() {
     }
   }, []);
 
+  //Iterating through the JSON data
   for (let i = 0; i < jsonData.length; i++) {
+    //Finding the data of the current user and checking if they have any translations
     if (
       currUser === jsonData[i].username &&
       jsonData[i].translations !== null
     ) {
-      //Key of the element is the array index from map - not the best solution but it gets the job done for now
+      //Iterating through the translations with the map hof, creating a list element with latest 10 translations
+        //Key of the element is the array index from map - not the best solution but it gets the job done for now
+        //Reversing the translations to start from the latest index. Helps when getting the latest 10 translations
       const listTrans = jsonData[i].translations
         .toReversed()
         .map((translation, index) =>
@@ -36,7 +44,6 @@ function DisplayUser() {
       return (
         <>
           <ul>
-            UserID: {sessionStorage.getItem("currID")}
             {listTrans}
           </ul>
           <button className="orange-btn" onClick={DeleteTrans}>Clear Record</button>
