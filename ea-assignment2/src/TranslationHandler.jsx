a95e1b728938bdbaf30e0a50de5c95462d31091b
AnnikaZar <annika.zarske@se.experis.com>
2023-07-19 08:46:27 +0200

    box on login
    feat: translation box visible on login as well

--- ea-assignment2/src/TranslationHandler.jsx
+++ ea-assignment2/src/TranslationHandler.jsx
@@ -23,6 +23,7 @@ const RenderDiv = () => {
 RenderDiv()
 
 function TranslationHandler(){
+    RenderDiv()
     const [ text, setText ] = useState({value: ""})
     let textToTranslate = text.value
     let compareArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
